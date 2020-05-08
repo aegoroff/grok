35671d75bb68e787926cb57744383fcecfe146b4
egoroff <egoroff@gmail.com>
2020-05-08 08:37:20 +0300

    grep mode by default

--- src/grok/configuration.h
+++ src/grok/configuration.h
@@ -16,8 +16,8 @@
 #define GROK_CONFIGURATION_H_
 
 typedef struct configuration_ctx_t {
-    void (*on_string)(struct arg_file* files, const char* const macro, const char* const str, int grep_mode);
-    void (*on_file)(struct arg_file* files, const char* const macro, const char* const path, int grep_mode);
+    void (*on_string)(struct arg_file* files, const char* const macro, const char* const str, int info_mode);
+    void (*on_file)(struct arg_file* files, const char* const macro, const char* const path, int info_mode);
     int argc;
     const char* const* argv;
 } configuration_ctx_t;
