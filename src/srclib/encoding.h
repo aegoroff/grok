adce8b99c71c5e102273eb8cd2ae2060912d0f09
egoroff <egoroff@gmail.com>
2020-05-10 22:03:48 +0300

    Copyright corrected

--- src/srclib/encoding.h
+++ src/srclib/encoding.h
@@ -6,7 +6,7 @@
  * \date    \verbatim
             Creation date: 2011-03-06
             \endverbatim
- * Copyright: (c) Alexander Egorov 2009-2020
+ * Copyright: (c) Alexander Egorov 2015-2020
  */
 
 #ifndef PCTRL_ENCODING_H_
