dd747be58cb096872c1e40d20beefa41fb100bfa
egoroff <egoroff@gmail.com>
2020-06-14 12:21:42 +0300

    Versioning change

--- src/grok/targetver.h
+++ src/grok/targetver.h
@@ -9,7 +9,7 @@
 #include <SDKDDKVer.h>
 #endif
 
-#define PRODUCT_VERSION "1.0.0.1"
+#define PRODUCT_VERSION "0.1.0"
 #define PROGRAM_NAME "grok"
 #define APP_NAME "Grok regexp macro processor " PRODUCT_VERSION
 #define PROG_EXE PROGRAM_NAME ".exe"
