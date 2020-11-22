4197dd3f0366ca2b0d3a57aa0f5cd8f318fcfcdb
egoroff <egoroff@gmail.com>
2020-11-23 00:52:27 +0300

    SizeToString test rewritten

--- src/_tst/SizeToString.cpp
+++ src/_tst/SizeToString.cpp
@@ -10,46 +10,49 @@
  */
 
 #include <cstdio>
+#include <cstring>
 #include <cstdint>
-#include "SizeToString.h"
 #include "catch.hpp"
 #include "lib.h"
 
 const size_t kBufferSize = 128;
 
-SizeToString::SizeToString() : BufferedTest(kBufferSize) {}
+TEST_CASE("size to string") {
+    std::unique_ptr<char> buffer = std::unique_ptr<char>(new char[kBufferSize]);
+    memset(buffer.get(), 0, kBufferSize);
 
-TEST_CASE_METHOD(SizeToString, "size to string / 1KB to string") {
-    const uint64_t size = 1024;
-    lib_size_to_string(size, GetBuffer());
+    SECTION("1KB boundary to string") {
+        const uint64_t size = 1024;
+        lib_size_to_string(size, buffer.get());
 
-    REQUIRE("1.00 Kb (1024 bytes)" == std::string(GetBuffer()));
-}
+        REQUIRE("1.00 Kb (1024 bytes)" == std::string(buffer.get()));
+    }
 
-TEST_CASE_METHOD(SizeToString, "size to string / 2KB + 10 bytes to string") {
-    const uint64_t size = BINARY_THOUSAND * 2 + 10;
-    lib_size_to_string(size, GetBuffer());
+    SECTION("2KB + 10 bytes to string") {
+        const uint64_t size = BINARY_THOUSAND * 2 + 10;
+        lib_size_to_string(size, buffer.get());
 
-    REQUIRE("2.01 Kb (2058 bytes)" == std::string(GetBuffer()));
-}
+        REQUIRE("2.01 Kb (2058 bytes)" == std::string(buffer.get()));
+    }
 
-TEST_CASE_METHOD(SizeToString, "size to string / bytes to string") {
-    const uint64_t size = 20;
-    lib_size_to_string(size, GetBuffer());
+    SECTION("bytes to string") {
+        const uint64_t size = 20;
+        lib_size_to_string(size, buffer.get());
 
-    REQUIRE("20 bytes" == std::string(GetBuffer()));
-}
+        REQUIRE("20 bytes" == std::string(buffer.get()));
+    }
 
-TEST_CASE_METHOD(SizeToString, "size to string / zero bytes to string") {
-    const uint64_t size = 0;
-    lib_size_to_string(size, GetBuffer());
+    SECTION("zero bytes to string") {
+        const uint64_t size = 0;
+        lib_size_to_string(size, buffer.get());
 
-    REQUIRE("0 bytes" == std::string(GetBuffer()));
-}
+        REQUIRE("0 bytes" == std::string(buffer.get()));
+    }
 
-TEST_CASE_METHOD(SizeToString, "size to string / max value to string") {
-    const uint64_t size = UINT64_MAX;
-    lib_size_to_string(size, GetBuffer());
+    SECTION("max value to string") {
+        const uint64_t size = UINT64_MAX;
+        lib_size_to_string(size, buffer.get());
 
-    REQUIRE("16.00 Eb (18446744073709551615 bytes)" == std::string(GetBuffer()));
+        REQUIRE("16.00 Eb (18446744073709551615 bytes)" == std::string(buffer.get()));
+    }
 }
