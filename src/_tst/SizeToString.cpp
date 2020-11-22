8b33e0c876f33621def672ce40decf54c1989ee7
egoroff <egoroff@gmail.com>
2020-11-23 00:21:51 +0300

    test names systematized

--- src/_tst/SizeToString.cpp
+++ src/_tst/SizeToString.cpp
@@ -19,35 +19,35 @@ const size_t kBufferSize = 128;
 
 SizeToString::SizeToString() : BufferedTest(kBufferSize) {}
 
-TEST_CASE_METHOD(SizeToString, "1KB to string", "[TSizeToString]") {
+TEST_CASE_METHOD(SizeToString, "size to string / 1KB to string") {
     const uint64_t size = 1024;
     lib_size_to_string(size, GetBuffer());
 
     REQUIRE("1.00 Kb (1024 bytes)" == std::string(GetBuffer()));
 }
 
-TEST_CASE_METHOD(SizeToString, "2KB + 10 bytes to string", "[TSizeToString]") {
+TEST_CASE_METHOD(SizeToString, "size to string / 2KB + 10 bytes to string") {
     const uint64_t size = BINARY_THOUSAND * 2 + 10;
     lib_size_to_string(size, GetBuffer());
 
     REQUIRE("2.01 Kb (2058 bytes)" == std::string(GetBuffer()));
 }
 
-TEST_CASE_METHOD(SizeToString, "bytes to string", "[TSizeToString]") {
+TEST_CASE_METHOD(SizeToString, "size to string / bytes to string") {
     const uint64_t size = 20;
     lib_size_to_string(size, GetBuffer());
 
     REQUIRE("20 bytes" == std::string(GetBuffer()));
 }
 
-TEST_CASE_METHOD(SizeToString, "zero bytes to string", "[TSizeToString]") {
+TEST_CASE_METHOD(SizeToString, "size to string / zero bytes to string") {
     const uint64_t size = 0;
     lib_size_to_string(size, GetBuffer());
 
     REQUIRE("0 bytes" == std::string(GetBuffer()));
 }
 
-TEST_CASE_METHOD(SizeToString, "max value to string", "[TSizeToString]") {
+TEST_CASE_METHOD(SizeToString, "size to string / max value to string") {
     const uint64_t size = UINT64_MAX;
     lib_size_to_string(size, GetBuffer());
 
