5345832ac465ba7436a06d77da7a9c7442b2ebc1
egoroff <egoroff@gmail.com>
2020-11-28 09:18:51 +0300

    time to string tests improved

--- src/_tst/time_to_string.cpp
+++ src/_tst/time_to_string.cpp
@@ -19,64 +19,77 @@ SCENARIO("time to string") {
     std::unique_ptr<char> buffer = std::unique_ptr<char>(new char[kBufferSize]);
     memset(buffer.get(), 0, kBufferSize);
 
-    WHEN("big time value that more then year") {
+    GIVEN( "50 000 001.0 seconds value" ) {
         const auto time = 50000001.0;
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, buffer.get());
 
-        THEN("result more then year") {
-            REQUIRE("1 years 213 days 16 hr 53 min 21.000 sec" == std::string(buffer.get()));
+        WHEN("normalize it and convert to string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, buffer.get());
+
+            THEN("result more then year") {
+                REQUIRE("1 years 213 days 16 hr 53 min 21.000 sec" == std::string(buffer.get()));
+            }
         }
     }
 
-    WHEN("big time value several days") {
+    GIVEN( "500 001.0 seconds value" ) {
         const auto time = 500001.0;
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, buffer.get());
 
-        THEN("result more then 5 days") {
-            REQUIRE("5 days 18 hr 53 min 21.000 sec" == std::string(buffer.get()));
+        WHEN("normalize it and convert to string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, buffer.get());
+
+            THEN("result more then 5 days") {
+                REQUIRE("5 days 18 hr 53 min 21.000 sec" == std::string(buffer.get()));
+            }
         }
     }
 
-    WHEN("hours time value") {
+    GIVEN( "7000.0 seconds value" ) {
         const auto time = 7000.0;
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, buffer.get());
 
-        THEN("result more then hour") {
-            REQUIRE("1 hr 56 min 40.000 sec" == std::string(buffer.get()));
+        WHEN("normalize it and convert to string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, buffer.get());
+
+            THEN("result more then hour") {
+                REQUIRE("1 hr 56 min 40.000 sec" == std::string(buffer.get()));
+            }
         }
     }
 
-    WHEN("minutes time value") {
+    GIVEN( "200.0 seconds value" ) {
         auto time = 200.0;
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, buffer.get());
 
-        THEN("result several minutes") {
-            REQUIRE("3 min 20.000 sec" == std::string(buffer.get()));
-            REQUIRE(time == result.total_seconds);
+        WHEN("normalize it and convert to string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, buffer.get());
+
+            THEN("result several minutes") {
+                REQUIRE("3 min 20.000 sec" == std::string(buffer.get()));
+                REQUIRE(time == result.total_seconds);
+            }
         }
     }
 
-    WHEN("seconds time value") {
+    GIVEN( "20.0 seconds value" ) {
         const auto time = 20.0;
 
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, buffer.get());
+        WHEN("normalize it and convert to string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, buffer.get());
 
-        THEN("result several seconds") {
-            REQUIRE("20.000 sec" == std::string(buffer.get()));
+            THEN("result same seconds as input") {
+                REQUIRE("20.000 sec" == std::string(buffer.get()));
+            }
         }
-    }
 
-    WHEN("null string") {
-        const auto time = 20.0;
-        auto result = lib_normalize_time(time);
-        lib_time_to_string(&result, nullptr);
+        WHEN("normalize it and try to convert it to null string") {
+            auto result = lib_normalize_time(time);
+            lib_time_to_string(&result, nullptr);
 
-        THEN("no crash") {
+            THEN("no crash") {
+            }
         }
     }
 }
