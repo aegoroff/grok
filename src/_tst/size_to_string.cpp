39b988bd5f95e2fc1747f0272821fbd72c2fffb8
egoroff <egoroff@gmail.com>
2020-11-28 08:59:01 +0300

    size to string tests rewritten in BDD style

--- src/_tst/size_to_string.cpp
+++ src/_tst/size_to_string.cpp
@@ -17,42 +17,52 @@
 
 const size_t kBufferSize = 128;
 
-TEST_CASE("size to string") {
+SCENARIO("size to string") {
     std::unique_ptr<char> buffer = std::unique_ptr<char>(new char[kBufferSize]);
     memset(buffer.get(), 0, kBufferSize);
 
-    SECTION("1KB boundary to string") {
+    WHEN("1024 value to string") {
         const uint64_t size = 1024;
         lib_size_to_string(size, buffer.get());
 
-        REQUIRE("1.00 Kb (1024 bytes)" == std::string(buffer.get()));
+        THEN("exactly 1 KB result with bytes after it in parens") {
+            REQUIRE("1.00 Kb (1024 bytes)" == std::string(buffer.get()));
+        }
     }
 
-    SECTION("2KB + 10 bytes to string") {
+    WHEN("2048 + 10 bytes to string") {
         const uint64_t size = BINARY_THOUSAND * 2 + 10;
         lib_size_to_string(size, buffer.get());
 
-        REQUIRE("2.01 Kb (2058 bytes)" == std::string(buffer.get()));
+        THEN("tiny more then 2 KB result with bytes after it in parens") {
+            REQUIRE("2.01 Kb (2058 bytes)" == std::string(buffer.get()));
+        }
     }
 
-    SECTION("bytes to string") {
+    WHEN("bytes to string") {
         const uint64_t size = 20;
         lib_size_to_string(size, buffer.get());
 
-        REQUIRE("20 bytes" == std::string(buffer.get()));
+        THEN("only bytes value output") {
+            REQUIRE("20 bytes" == std::string(buffer.get()));
+        }
     }
 
-    SECTION("zero bytes to string") {
+    WHEN("zero bytes to string") {
         const uint64_t size = 0;
         lib_size_to_string(size, buffer.get());
 
-        REQUIRE("0 bytes" == std::string(buffer.get()));
+        THEN("0 bytes output") {
+            REQUIRE("0 bytes" == std::string(buffer.get()));
+        }
     }
 
-    SECTION("max value to string") {
+    WHEN("max value to string") {
         const uint64_t size = UINT64_MAX;
         lib_size_to_string(size, buffer.get());
 
-        REQUIRE("16.00 Eb (18446744073709551615 bytes)" == std::string(buffer.get()));
+        THEN("16 EB output") {
+            REQUIRE("16.00 Eb (18446744073709551615 bytes)" == std::string(buffer.get()));
+        }
     }
 }
