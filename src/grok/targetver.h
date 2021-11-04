faadb904afb22fc3e717e48b7a07c75205983ce4
egoroff <egoroff@gmail.com>
2021-11-04 14:41:19 +0300

    ci: v0.2.5

--- src/grok/targetver.h
+++ src/grok/targetver.h
@@ -9,7 +9,7 @@
 #include <SDKDDKVer.h>
 #endif
 
-#define PRODUCT_VERSION "0.2.4"
+#define PRODUCT_VERSION "0.2.5"
 #define PROGRAM_NAME "grok"
 #define APP_NAME "Grok regexp macro processor " PRODUCT_VERSION
 #ifdef _MSC_VER
