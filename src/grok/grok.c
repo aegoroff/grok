b6a279d1c93a96310a8331d04cb4cbaff74eeece
egoroff <egoroff@gmail.com>
2021-11-10 22:06:50 +0300

    security: replace strlen by strnlen in code that defines real exe path

--- src/grok/grok.c
+++ src/grok/grok.c
@@ -29,6 +29,7 @@
 #ifdef __APPLE_CC__
 
 #include <mach-o/dyld.h>
+#include <sys/syslimits.h>
 
 #endif
 
@@ -393,7 +394,7 @@ const char* grok_get_executable_path(apr_pool_t* pool) {
         } else {
             char* real_path = realpath(buf, NULL);
             if(real_path != NULL) {
-                size_t len = strlen(real_path);
+                size_t len = strnlen(real_path, PATH_MAX);
                 buf = (char*) apr_pcalloc(pool, len + 1);
                 memcpy(buf, real_path, len);
                 free(real_path);
