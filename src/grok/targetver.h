f4fea0af2af9854bfd36247f2f586a131c4fde53
egoroff <egoroff@gmail.com>
2022-01-19 09:16:37 +0300

    ci: Grok version definition using Environment

--- src/grok/targetver.h
+++ src/grok/targetver.h
@@ -9,7 +9,10 @@
 #include <SDKDDKVer.h>
 #endif
 
+#ifndef PRODUCT_VERSION
 #define PRODUCT_VERSION "0.2.8"
+#endif
+
 #define PROGRAM_NAME "grok"
 #define APP_NAME "Grok regexp macro processor " PRODUCT_VERSION
 #ifdef _MSC_VER
