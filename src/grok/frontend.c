d179a8d8e7f049d9c5b2543b76355845adc9412b
Alexander Egorov <egoroff@gmail.com>
2023-12-12 21:43:01 +0300

    style: llvm style applied

--- src/grok/frontend.c
+++ src/grok/frontend.c
@@ -9,18 +9,17 @@
  * Copyright: (c) Alexander Egorov 2015-2023
  */
 
-
-#include <stdio.h>
 #include "frontend.h"
-#include "apr_tables.h"
 #include "apr_pools.h"
 #include "apr_strings.h"
+#include "apr_tables.h"
+#include <stdio.h>
 
-#define ARRAY_INIT_SZ   256
+#define ARRAY_INIT_SZ 256
 
 typedef struct macro {
-    char* name;
-    char* property;
+    char *name;
+    char *property;
 } macro_t;
 
 /*
@@ -29,61 +28,51 @@ typedef struct macro {
 */
 
 // Forwards
-void prfend_add_part(char* data, char* reference, part_t type);
+void prfend_add_part(char *data, char *reference, part_t type);
 
-static apr_pool_t* fend_pool = NULL;
-static apr_hash_t* fend_definition = NULL;
-static apr_array_header_t* fend_composition = NULL;
+static apr_pool_t *fend_pool = NULL;
+static apr_hash_t *fend_definition = NULL;
+static apr_array_header_t *fend_composition = NULL;
 
-void fend_init(apr_pool_t* p) {
+void fend_init(apr_pool_t *p) {
     fend_pool = p;
     fend_definition = apr_hash_make(fend_pool);
 }
 
-void fend_on_definition(void) {
-    fend_composition = apr_array_make(fend_pool, ARRAY_INIT_SZ, sizeof(info_t*));
-}
+void fend_on_definition(void) { fend_composition = apr_array_make(fend_pool, ARRAY_INIT_SZ, sizeof(info_t *)); }
 
-void fend_on_definition_end(char* key) {
-    apr_array_header_t* parts = apr_array_make(fend_pool, fend_composition->nelts, sizeof(info_t*));
-    for(size_t i = 0; i < fend_composition->nelts; i++) {
-        *(info_t**) apr_array_push(parts) = ((info_t**) fend_composition->elts)[i];
+void fend_on_definition_end(char *key) {
+    apr_array_header_t *parts = apr_array_make(fend_pool, fend_composition->nelts, sizeof(info_t *));
+    for (size_t i = 0; i < fend_composition->nelts; i++) {
+        *(info_t **)apr_array_push(parts) = ((info_t **)fend_composition->elts)[i];
     }
-    apr_hash_set(fend_definition, (const char*) key, APR_HASH_KEY_STRING, parts);
+    apr_hash_set(fend_definition, (const char *)key, APR_HASH_KEY_STRING, parts);
 }
 
-void fend_on_literal(char* str) {
-    prfend_add_part(str, NULL, part_literal);
-}
+void fend_on_literal(char *str) { prfend_add_part(str, NULL, part_literal); }
 
-void fend_on_grok(macro_t* macro) {
-    prfend_add_part(macro->name, macro->property, part_reference);
-}
+void fend_on_grok(macro_t *macro) { prfend_add_part(macro->name, macro->property, part_reference); }
 
-macro_t* fend_on_macro(char* name, char* prop) {
-    macro_t* result = (macro_t*) apr_pcalloc(fend_pool, sizeof(macro_t));
+macro_t *fend_on_macro(char *name, char *prop) {
+    macro_t *result = (macro_t *)apr_pcalloc(fend_pool, sizeof(macro_t));
     result->name = name;
     result->property = prop;
     return result;
 }
 
-void prfend_add_part(char* data, char* reference, part_t type) {
-    info_t* result = (info_t*) apr_pcalloc(fend_pool, sizeof(info_t));
+void prfend_add_part(char *data, char *reference, part_t type) {
+    info_t *result = (info_t *)apr_pcalloc(fend_pool, sizeof(info_t));
     result->type = type;
     result->data = data;
     result->reference = reference;
-    *(info_t**) apr_array_push(fend_composition) = result;
+    *(info_t **)apr_array_push(fend_composition) = result;
 }
 
-apr_array_header_t* fend_get_pattern(const char* def) {
-    apr_array_header_t* parts = apr_hash_get(fend_definition, def, APR_HASH_KEY_STRING);
+apr_array_header_t *fend_get_pattern(const char *def) {
+    apr_array_header_t *parts = apr_hash_get(fend_definition, def, APR_HASH_KEY_STRING);
     return parts;
 }
 
-char* fend_strdup(char* str) {
-    return apr_pstrdup(fend_pool, str);
-}
+char *fend_strdup(char *str) { return apr_pstrdup(fend_pool, str); }
 
-apr_hash_t* fend_get_patterns(void) {
-    return fend_definition;
-}
+apr_hash_t *fend_get_patterns(void) { return fend_definition; }
--- src/srclib/dbg_helpers.c
+++ src/srclib/dbg_helpers.c
@@ -9,9 +9,9 @@
  * Copyright: (c) Alexander Egorov 2015-2023
  */
 
+#include "dbg_helpers.h"
 #include "targetver.h"
 #include <stdio.h>
-#include "dbg_helpers.h"
 
 #ifdef _MSC_VER
 
@@ -25,10 +25,10 @@
    prdbg_ - private members
 */
 
-void prdbg_print_win32_error(const char* message);
+void prdbg_print_win32_error(const char *message);
 
-LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS* p_exception_info) {
-    LONG result = EXCEPTION_CONTINUE_SEARCH;    // finalize process in standard way by default
+LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS *p_exception_info) {
+    LONG result = EXCEPTION_CONTINUE_SEARCH; // finalize process in standard way by default
     HMODULE hDll = NULL;
     MINIDUMP_EXCEPTION_INFORMATION exInfo = {0};
     BOOL isOK = FALSE;
@@ -37,26 +37,20 @@ LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS* p_exception_info) {
 
     hDll = LoadLibraryA(DBG_HELP_DLL);
 
-    if(hDll == NULL) {
+    if (hDll == NULL) {
         prdbg_print_win32_error(" Cannot load dll " DBG_HELP_DLL);
         return result;
     }
     // get func address
-    pfnDump = (MINIDUMPWRITEDUMP) GetProcAddress(hDll, DUMP_FUNCTION);
-    if(!pfnDump) {
+    pfnDump = (MINIDUMPWRITEDUMP)GetProcAddress(hDll, DUMP_FUNCTION);
+    if (!pfnDump) {
         prdbg_print_win32_error(" Cannot get address of " DUMP_FUNCTION " function");
         return result;
     }
 
-    hFile = CreateFileA(DUMP_FILE_NAME,
-                        GENERIC_WRITE,
-                        0,
-                        NULL,
-                        CREATE_ALWAYS,
-                        FILE_ATTRIBUTE_NORMAL,
-                        NULL);
+    hFile = CreateFileA(DUMP_FILE_NAME, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 
-    if(hFile == INVALID_HANDLE_VALUE) {
+    if (hFile == INVALID_HANDLE_VALUE) {
         prdbg_print_win32_error(UNHANDLED_EXCEPTION_OCCURRED "Error on creating dump file: " DUMP_FILE_NAME);
         return result;
     }
@@ -66,9 +60,8 @@ LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS* p_exception_info) {
     exInfo.ClientPointers = 0;
 
     // Write pDumpFile
-    isOK = pfnDump(GetCurrentProcess(),
-                   GetCurrentProcessId(), hFile, MiniDumpNormal, &exInfo, NULL, NULL);
-    if(isOK) {
+    isOK = pfnDump(GetCurrentProcess(), GetCurrentProcessId(), hFile, MiniDumpNormal, &exInfo, NULL, NULL);
+    if (isOK) {
         printf_s(UNHANDLED_EXCEPTION_OCCURRED "Dump saved to: %s", DUMP_FILE_NAME);
         result = EXCEPTION_EXECUTE_HANDLER;
     } else {
@@ -78,22 +71,18 @@ LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS* p_exception_info) {
     return result;
 }
 
-void prdbg_print_win32_error(const char* message) {
+void prdbg_print_win32_error(const char *message) {
     DWORD errorCode = 0;
-    void* buffer = NULL;
+    void *buffer = NULL;
 
-    __try
-    {
+    __try {
         errorCode = GetLastError();
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS |
-                       FORMAT_MESSAGE_MAX_WIDTH_MASK | FORMAT_MESSAGE_ALLOCATE_BUFFER,
-                       NULL, errorCode, MAKELANGID(LANG_NEUTRAL,
-                                                   SUBLANG_DEFAULT), (char*) &buffer, 0, NULL);
-        printf_s("%s. Windows error %#x: %s", message, errorCode, (char*) buffer);
-    }
-    __finally
-    {
-        if(buffer != NULL) {
+        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK |
+                           FORMAT_MESSAGE_ALLOCATE_BUFFER,
+                       NULL, errorCode, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (char *)&buffer, 0, NULL);
+        printf_s("%s. Windows error %#x: %s", message, errorCode, (char *)buffer);
+    } __finally {
+        if (buffer != NULL) {
             LocalFree(buffer);
         }
     }
