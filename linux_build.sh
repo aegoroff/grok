b5cc41ff6d2421e13088e5ffe57845656db70649
Alexander Egorov <egoroff@gmail.com>
2023-12-31 14:39:15 +0300

    ci: experimental cross compilation suport on linux

--- linux_build.sh
+++ linux_build.sh
@@ -1,10 +1,12 @@
 BUILD_CONF=Release
 ABI=$1
+OS=$2
 [[ -n "${ABI}" ]] || ABI=musl
-BUILD_DIR=build-${ABI}-${BUILD_CONF}
+[[ -n "${OS}" ]] || OS=linux
+BUILD_DIR=build-${OS}-${ABI}-${BUILD_CONF}
 LIB_INSTALL_SRC=./external_lib/src
 LIB_INSTALL_PREFIX=./external_lib/lib
-CC_FLAGS="zig cc -target x86_64-linux-${ABI}"
+CC_FLAGS="zig cc -target x86_64-${OS}-${ABI}"
 CFLAGS="-Ofast -march=haswell -mtune=haswell"
 APR_SRC=apr-1.7.4
 APR_UTIL_SRC=apr-util-1.6.3
@@ -24,21 +26,24 @@ APR_PREFIX=${EXTERNAL_PREFIX}/apr
 echo ${EXPAT_PREFIX}
 echo ${APR_PREFIX}
 
+if [[ "${ABI}" = "musl" ]]; then
+    TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=$(realpath cmake/zig-toolchain-linux-musl.cmake)"
+fi
+if [[ "${ABI}" = "none" ]]; then
+    TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=$(realpath cmake/zig-toolchain-macos-none.cmake)"
+fi
+
 (cd ${LIB_INSTALL_SRC} && [[ -f "${EXPAT_SRC}.tar.gz" ]] || wget https://github.com/libexpat/libexpat/releases/download/R_2_5_0/${EXPAT_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${EXPAT_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${EXPAT_SRC} && CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${EXPAT_PREFIX} && make -j && make install)
+(cd ${LIB_INSTALL_SRC} && tar -xzf ${EXPAT_SRC}.tar.gz)
+(cd ${LIB_INSTALL_SRC}/${EXPAT_SRC} && cmake ${TOOLCHAIN} -B ${BUILD_DIR} -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=${BUILD_CONF} -DEXPAT_BUILD_TESTS=False -DCMAKE_INSTALL_PREFIX=${EXPAT_PREFIX} && cmake --build ${BUILD_DIR} && cmake --install ${BUILD_DIR})
 
 (cd ${LIB_INSTALL_SRC} && [[ -f "${APR_SRC}.tar.gz" ]] || wget https://dlcdn.apache.org/apr/${APR_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${APR_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${APR_SRC} && CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${APR_PREFIX} && make -j && make install)
+(cd ${LIB_INSTALL_SRC} && tar -xzf ${APR_SRC}.tar.gz)
+(cd ${LIB_INSTALL_SRC}/${APR_SRC} && CC="${CC_FLAGS}" CFLAGS="${CFLAGS} -Wno-implicit-function-declaration -Wno-int-conversion" ./configure ac_cv_file__dev_zero=yes apr_cv_process_shared_works=yes apr_cv_mutex_robust_shared=yes apr_cv_tcp_nodelay_with_cork=yes --host=x86_64-linux --enable-shared=no --prefix=${APR_PREFIX} && make -j && make install)
 
 (cd ${LIB_INSTALL_SRC} && [[ -f "${APR_UTIL_SRC}.tar.gz" ]] || wget https://dlcdn.apache.org/apr/${APR_UTIL_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${APR_UTIL_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${APR_UTIL_SRC} && CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${APR_PREFIX} --with-apr=${APR_PREFIX} --with-expat=${EXPAT_PREFIX} && make -j && make install)
-
-if [[ "${ABI}" = "musl" ]]; then
-    TOOLCHAIN="-DCMAKE_TOOLCHAIN_FILE=cmake/zig-toolchain-linux-musl.cmake"
-fi
+(cd ${LIB_INSTALL_SRC} && tar -xzf ${APR_UTIL_SRC}.tar.gz)
+(cd ${LIB_INSTALL_SRC}/${APR_UTIL_SRC} && CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --host=x86_64-linux --enable-shared=no --prefix=${APR_PREFIX} --with-apr=${APR_PREFIX} --with-expat=${EXPAT_PREFIX} && make -j && make install)
 
 APR_INCLUDE="${EXTERNAL_PREFIX}/apr/include/apr-1" \
 APR_LINK="${EXTERNAL_PREFIX}/apr/lib" \
