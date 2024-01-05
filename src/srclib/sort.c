23ef3ad3ad70afd273d1540baf4c1a5cd8839c5b
Alexander Egorov <egoroff@gmail.com>
2024-01-05 17:10:01 +0300

    chore: bump Copyright year

--- src/srclib/sort.c
+++ src/srclib/sort.c
@@ -6,7 +6,7 @@
  * \date    \verbatim
             Creation date: 2020-11-21
             \endverbatim
- * Copyright: (c) Alexander Egorov 2015-2023
+ * Copyright: (c) Alexander Egorov 2015-2024
  */
 
 #include "sort.h"
