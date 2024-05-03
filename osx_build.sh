28ee1c07b6c460f7e33e615674c7ca0dfaa77ec6
Alexander Egorov <egoroff@gmail.com>
2024-05-03 23:54:43 +0300

    build: remove PCRE sources from project code. Building it as external lib

--- osx_build.sh
+++ osx_build.sh
@@ -10,6 +10,7 @@ APR_SRC=apr-1.7.4
 APR_UTIL_SRC=apr-util-1.6.3
 EXPAT_VER=2.6.2
 EXPAT_SRC=expat-${EXPAT_VER}
+PCRE_SRC=pcre2-10.43
 
 [[ -d "${LIB_INSTALL_SRC}" ]] || mkdir -p ${LIB_INSTALL_SRC}
 [[ -d "${LIB_INSTALL_PREFIX}" ]] && rm -rf ${LIB_INSTALL_PREFIX}
@@ -26,6 +27,7 @@ realpath() {
 EXTERNAL_PREFIX=$(realpath ${LIB_INSTALL_PREFIX})
 EXPAT_PREFIX=${EXTERNAL_PREFIX}/expat
 APR_PREFIX=${EXTERNAL_PREFIX}/apr
+PCRE_PREFIX=${EXTERNAL_PREFIX}/pcre
 echo ${EXPAT_PREFIX}
 echo ${APR_PREFIX}
 
@@ -40,6 +42,10 @@ echo ${APR_PREFIX}
 (cd ${LIB_INSTALL_SRC} && [[ -f "${APR_UTIL_SRC}.tar.gz" ]] || curl -O -L https://dlcdn.apache.org/apr/${APR_UTIL_SRC}.tar.gz)
 (cd ${LIB_INSTALL_SRC} && tar -xvzf ${APR_UTIL_SRC}.tar.gz)
 (cd ${LIB_INSTALL_SRC}/${APR_UTIL_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${APR_PREFIX} --with-apr=${APR_PREFIX} --with-expat=${EXPAT_PREFIX} && make -j $(sysctl -n hw.ncpu) && make install)
+
+(cd ${LIB_INSTALL_SRC} && [[ -f "${PCRE_SRC}.tar.gz" ]] || wget https://github.com/PCRE2Project/pcre2/releases/download/${PCRE_SRC}/${PCRE_SRC}.tar.gz)
+(cd ${LIB_INSTALL_SRC} && tar -xzf ${PCRE_SRC}.tar.gz)
+(cd ${LIB_INSTALL_SRC}/${PCRE_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --prefix=${PCRE_PREFIX} --enable-shared=no && make -j $(sysctl -n hw.ncpu) && make install)
 
 cmake -DCMAKE_BUILD_TYPE=${BUILD_CONF} -B ${BUILD_DIR}
 cmake --build ${BUILD_DIR} --parallel $(sysctl -n hw.ncpu)
