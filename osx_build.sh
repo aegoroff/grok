d1501dfaa5ac76c9a332e51eb70f9a282c30450c
Alexander Egorov <egoroff@gmail.com>
2024-06-08 17:57:45 +0300

    style: more bash script warnings fixed

--- osx_build.sh
+++ osx_build.sh
@@ -31,30 +31,28 @@ EXPAT_PREFIX=${EXTERNAL_PREFIX}/expat
 APR_PREFIX=${EXTERNAL_PREFIX}/apr
 PCRE_PREFIX=${EXTERNAL_PREFIX}/pcre
 ARGTABLE3_PREFIX=${EXTERNAL_PREFIX}/argtable3
-echo ${EXPAT_PREFIX}
-echo ${APR_PREFIX}
 
-(cd ${LIB_INSTALL_SRC} && [[ -f "${EXPAT_SRC}.tar.gz" ]] || curl -O -L https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VER//./_}/${EXPAT_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${EXPAT_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${EXPAT_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${EXPAT_PREFIX} && make -j $(sysctl -n hw.ncpu) && make install)
+(cd "${LIB_INSTALL_SRC}" && [[ -f "${EXPAT_SRC}.tar.gz" ]] || curl -O -L https://github.com/libexpat/libexpat/releases/download/R_${EXPAT_VER//./_}/${EXPAT_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}" && tar -xvzf ${EXPAT_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}/${EXPAT_SRC}" && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix="${EXPAT_PREFIX}" && make -j $(sysctl -n hw.ncpu) && make install)
 
-(cd ${LIB_INSTALL_SRC} && [[ -f "${ARGTABLE3_SRC}.tar.gz" ]] || curl -O -L https://github.com/argtable/argtable3/releases/download/${ARGTABLE3_VER}/${ARGTABLE3_SRC}.tar.gz)
-[[ -d "${ARGTABLE3_PREFIX}" ]] || mkdir ${ARGTABLE3_PREFIX}
-(cd ${LIB_INSTALL_SRC} && tar -xzf ${ARGTABLE3_SRC}.tar.gz && cp -v ./dist/argtable3* ${ARGTABLE3_PREFIX}/)
+(cd "${LIB_INSTALL_SRC}" && [[ -f "${ARGTABLE3_SRC}.tar.gz" ]] || curl -O -L https://github.com/argtable/argtable3/releases/download/${ARGTABLE3_VER}/${ARGTABLE3_SRC}.tar.gz)
+[[ -d "${ARGTABLE3_PREFIX}" ]] || mkdir "${ARGTABLE3_PREFIX}"
+(cd "${LIB_INSTALL_SRC}" && tar -xzf ${ARGTABLE3_SRC}.tar.gz && cp -v ./dist/argtable3* "${ARGTABLE3_PREFIX}/")
 
-(cd ${LIB_INSTALL_SRC} && [[ -f "${APR_SRC}.tar.gz" ]] || curl -O -L https://dlcdn.apache.org/apr/${APR_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${APR_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${APR_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${APR_PREFIX} && make -j $(sysctl -n hw.ncpu) && make install)
+(cd "${LIB_INSTALL_SRC}" && [[ -f "${APR_SRC}.tar.gz" ]] || curl -O -L https://dlcdn.apache.org/apr/${APR_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}" && tar -xvzf ${APR_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}/${APR_SRC}" && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix="${APR_PREFIX}" && make -j $(sysctl -n hw.ncpu) && make install)
 
-(cd ${LIB_INSTALL_SRC} && [[ -f "${APR_UTIL_SRC}.tar.gz" ]] || curl -O -L https://dlcdn.apache.org/apr/${APR_UTIL_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xvzf ${APR_UTIL_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${APR_UTIL_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix=${APR_PREFIX} --with-apr=${APR_PREFIX} --with-expat=${EXPAT_PREFIX} && make -j $(sysctl -n hw.ncpu) && make install)
+(cd "${LIB_INSTALL_SRC}" && [[ -f "${APR_UTIL_SRC}.tar.gz" ]] || curl -O -L https://dlcdn.apache.org/apr/${APR_UTIL_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}" && tar -xvzf ${APR_UTIL_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}/${APR_UTIL_SRC}" && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" ./configure --enable-shared=no --prefix="${APR_PREFIX}" --with-apr="${APR_PREFIX}" --with-expat="${EXPAT_PREFIX}" && make -j $(sysctl -n hw.ncpu) && make install)
 
-(cd ${LIB_INSTALL_SRC} && [[ -f "${PCRE_SRC}.tar.gz" ]] || curl -O -L https://github.com/PCRE2Project/pcre2/releases/download/${PCRE_SRC}/${PCRE_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC} && tar -xzf ${PCRE_SRC}.tar.gz)
-(cd ${LIB_INSTALL_SRC}/${PCRE_SRC} && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --prefix=${PCRE_PREFIX} --enable-shared=no && make -j $(sysctl -n hw.ncpu) && make install)
+(cd "${LIB_INSTALL_SRC}" && [[ -f "${PCRE_SRC}.tar.gz" ]] || curl -O -L https://github.com/PCRE2Project/pcre2/releases/download/${PCRE_SRC}/${PCRE_SRC}.tar.gz)
+(cd "${LIB_INSTALL_SRC}" && tar -xzf "${PCRE_SRC}.tar.gz")
+(cd "${LIB_INSTALL_SRC}/${PCRE_SRC}" && AR="${AR_FLAGS}" RANLIB="${RANLIB_FLAGS}" CC="${CC_FLAGS}" CFLAGS="${CFLAGS}" CXXFLAGS="${CFLAGS}" ./configure --prefix="${PCRE_PREFIX}" --enable-shared=no && make -j $(sysctl -n hw.ncpu) && make install)
 
-cmake -DCMAKE_BUILD_TYPE=${BUILD_CONF} -B ${BUILD_DIR}
-cmake --build ${BUILD_DIR} --parallel $(sysctl -n hw.ncpu)
-ctest --test-dir ${BUILD_DIR} -VV
-(cd ${BUILD_DIR} && cpack --config CPackConfig.cmake)
+cmake -DCMAKE_BUILD_TYPE=${BUILD_CONF} -B "${BUILD_DIR}"
+cmake --build "${BUILD_DIR}" --parallel $(sysctl -n hw.ncpu)
+ctest --test-dir "${BUILD_DIR}" -VV
+(cd "${BUILD_DIR}" && cpack --config CPackConfig.cmake)
