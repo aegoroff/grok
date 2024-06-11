92b760227ab6d7211279b1e01e28b78bb53cca57
egoroff <egoroff@gmail.com>
2024-06-11 09:44:13 +0300

    Revert "style: sources files reformatted"
    This reverts commit c21373b12a578f4cd66cf043758f037e09315183.

--- src/srclib/dbg_helpers.h
+++ src/srclib/dbg_helpers.h
@@ -18,17 +18,22 @@ extern "C" {
 
 #ifdef _MSC_VER
 
-#include <Dbghelp.h>
 #include <windows.h>
+#include <Dbghelp.h>
 
-typedef BOOL(WINAPI *MINIDUMPWRITEDUMP)(HANDLE, DWORD, HANDLE, MINIDUMP_TYPE, PMINIDUMP_EXCEPTION_INFORMATION,
-                                        PMINIDUMP_USER_STREAM_INFORMATION, PMINIDUMP_CALLBACK_INFORMATION);
+typedef BOOL (WINAPI * MINIDUMPWRITEDUMP)
+    (HANDLE,
+    DWORD,
+    HANDLE,
+    MINIDUMP_TYPE,
+    PMINIDUMP_EXCEPTION_INFORMATION, PMINIDUMP_USER_STREAM_INFORMATION,
+    PMINIDUMP_CALLBACK_INFORMATION);
 
 /*!
  * \brief Application top level exception handler that creates (if it's possible) core dump
  * @param p_exception_info pointer to exception information
  */
-LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS *p_exception_info);
+LONG WINAPI dbg_top_level_filter(struct _EXCEPTION_POINTERS* p_exception_info);
 
 #endif
 
