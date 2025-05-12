bd08a8f220318757e9df4cb9f1f87dfd76f948d6
Alexander Egorov <egoroff@gmail.com>
2025-05-12 22:42:05 +0300

    build: run and test steps added into build.zig

--- build.zig
+++ build.zig
@@ -76,6 +76,38 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(lib);
     b.installArtifact(exe);
     b.installArtifact(tst);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const run_unit_tests = b.addRunArtifact(tst);
+    run_unit_tests.step.dependOn(b.getInstallStep());
+
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
 }
 
 const libgrok_sources = [_][]const u8{
