f6e963a9b0e8e56b30b9bf55061537f6f07809d2
Alexander Egorov <egoroff@gmail.com>
2025-12-24 19:01:01 +0300

    build: run step removed from zig build script

--- build.zig
+++ build.zig
@@ -87,29 +87,6 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
     b.installArtifact(tst);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
-    const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const run_unit_tests = b.addRunArtifact(tst);
     run_unit_tests.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
