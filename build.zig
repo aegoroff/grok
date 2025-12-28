d141d4960a225047963990e28dcd9fe084c08569
Alexander Egorov <egoroff@gmail.com>
2025-12-28 11:33:50 +0300

    build: flex + bison tools support on windows and macos

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -9,18 +10,65 @@ pub fn build(b: *std.Build) void {
     const version_opt = b.option([]const u8, "version", "The version of the app") orelse "0.3.0-dev";
     options.addOption([]const u8, "version", version_opt);
 
-    const flex_step = b.addSystemCommand(&[_][]const u8{
-        "flex",
-        "--fast",
-        "--outfile=src/grok/generated/grok.flex.c",
-        "src/grok/grok.lex",
-    });
-    const bison_step = b.addSystemCommand(&[_][]const u8{
-        "bison",
-        "--output=src/grok/generated/grok.tab.c",
-        "-dy",
-        "src/grok/grok.y",
-    });
+    const flex_input = "src/grok/grok.lex";
+    const flex_output = "--outfile=src/grok/generated/grok.flex.c";
+
+    var flex_args: []const []const u8 = undefined;
+    const os_tag = builtin.os.tag;
+    if (os_tag == .linux) {
+        flex_args = &[_][]const u8{
+            "flex",
+            "--fast",
+            flex_output,
+            flex_input,
+        };
+    } else if (os_tag == .windows) {
+        flex_args = &[_][]const u8{
+            "win_flex.exe",
+            "--fast",
+            "--wincompat",
+            flex_output,
+            flex_input,
+        };
+    } else if (os_tag == .macos) {
+        flex_args = &[_][]const u8{
+            "/usr/local/opt/flex/bin/flex",
+            "--fast",
+            flex_output,
+            flex_input,
+        };
+    }
+
+    const flex_step = b.addSystemCommand(flex_args);
+
+    const bison_input = "src/grok/grok.y";
+    const bison_output = "--output=src/grok/generated/grok.tab.c";
+
+    var bison_args: []const []const u8 = undefined;
+    if (os_tag == .linux) {
+        bison_args = &[_][]const u8{
+            "bison",
+            bison_output,
+            "-dy",
+            bison_input,
+        };
+    } else if (os_tag == .windows) {
+        bison_args = &[_][]const u8{
+            "win_bison.exe",
+            bison_output,
+            "-dy",
+            bison_input,
+        };
+    } else if (os_tag == .macos) {
+        bison_args = &[_][]const u8{
+            "/usr/local/opt/flex/bin/flex",
+            bison_output,
+            "-dy",
+            bison_input,
+        };
+    }
+
+    const bison_step = b.addSystemCommand(bison_args);
     bison_step.step.dependOn(&flex_step.step);
 
     const pcre2_dep = b.dependency("pcre2", .{
@@ -49,7 +97,6 @@ pub fn build(b: *std.Build) void {
     });
     exe.root_module.addImport("glob", glob_dep.module("glob"));
     exe.root_module.addImport("yazap", yazap.module("yazap"));
-    exe.root_module.addIncludePath(pcre.installed_headers.items[0].getSource().dirname());
     exe.root_module.addIncludePath(b.path("src/grok/generated"));
     exe.root_module.addIncludePath(b.path("src/srclib"));
     exe.root_module.addIncludePath(b.path("src"));
