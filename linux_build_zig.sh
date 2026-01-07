1f860c27fec11095231c717032193f151987c37d
Alexander Egorov <egoroff@gmail.com>
2026-01-07 16:27:55 +0300

    build: redundant build step run removed because it's being started by archive command too

--- linux_build_zig.sh
+++ linux_build_zig.sh
@@ -16,10 +16,8 @@ OPTIMIZE=ReleaseFast
 DCPU=""
 [[ -n "${CPU}" ]] && DCPU="-Dcpu=${CPU}"
 
-zig build -Doptimize=${OPTIMIZE} ${DCPU} -Dtarget="${ARCH}"-"${OS}"-"${ABI}" -Dversion="${VERSION}" --summary all --prefix-exe-dir "${ZIG_PREFIX_DIR}"
-
 if [[ "${ARCH}" = "x86_64" ]] && [[ "${OS}" = "linux" ]]; then
-  zig build test -Doptimize=${OPTIMIZE} "${DCPU}" -Dtarget="${ARCH}"-"${OS}"-"${ABI}" --summary all -- -s
+  zig build test -Doptimize=${OPTIMIZE} "${DCPU}" -Dtarget="${ARCH}"-"${OS}"-"${ABI}" -Dversion="${VERSION}" --summary all
 fi
 
 zig build archive -Doptimize=${OPTIMIZE} -Dtarget="${ARCH}"-"${OS}"-"${ABI}" -Dversion="${VERSION}" --summary all --prefix-exe-dir "${ZIG_PREFIX_DIR}"
