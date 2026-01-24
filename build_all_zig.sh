459be7906825d8fd802c3359a3410b5933b7ef91
Alexander Egorov <egoroff@gmail.com>
2026-01-24 08:18:02 +0300

    feat: windows aarch64 platform binary added to artefacts

--- build_all_zig.sh
+++ build_all_zig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ./zig-out/*.tar.gz
-for i in "x86_64 linux musl 0.3.0 haswell" "aarch64 linux musl 0.3.0" "x86_64 macos none 0.3.0 haswell" "aarch64 macos none 0.3.0 apple_m1" "x86_64 windows gnu 0.3.0 haswell"
+for i in "x86_64 linux musl 0.3.0 haswell" "aarch64 linux musl 0.3.0" "x86_64 macos none 0.3.0 haswell" "aarch64 macos none 0.3.0 apple_m1" "x86_64 windows gnu 0.3.0 haswell" "aarch64 windows gnu 0.3.0"
 do
     set -- $i # Convert the "tuple" into the param args $1 $2...
     echo "$1" - "$2" - "$3" - "$4" - "$5"
