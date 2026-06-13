e540246831c8abdfb24565d36c74a139d1005816
Alexander Egorov <egoroff@gmail.com>
2026-06-13 18:35:45 +0300

    ci: Allow version only from numbers dots or -

--- linux_build_zig.sh
+++ linux_build_zig.sh
@@ -8,7 +8,7 @@ CPU=$5
 [[ -n "${ABI}" ]] || ABI=musl
 [[ -n "${OS}" ]] || OS=linux
 [[ -n "${ARCH}" ]] || ARCH=x86_64
-[[ -z "${VERSION}" || "${VERSION}" != *merge ]] && VERSION="0.4.0-dev"
+[[ ! "${VERSION}" =~ ^[0-9.-]+$ ]] && VERSION="0.4.0-dev"
 
 ZIG_PREFIX_DIR=bin-${ARCH}-${OS}-${ABI}
 OPTIMIZE=ReleaseFast
