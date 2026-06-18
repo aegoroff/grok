ee39dfecb2f7edbf91388088bd07ec2a47caa026
Alexander Egorov <egoroff@gmail.com>
2026-06-18 22:32:15 +0300

    build: building simplified - all targets use static library for c code

--- build.zig
+++ build.zig
@@ -52,20 +52,6 @@ pub fn build(b: *std.Build) void {
     const bison = b.addSystemCommand(bison_args);
     bison.step.dependOn(&flex.step);
 
-    const c_lib = b.addLibrary(.{
-        .name = "grok-c",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
-    });
-    c_lib.root_module.addIncludePath(b.path(c_code_path));
-    c_lib.root_module.addIncludePath(b.path(generated_path));
-    c_lib.root_module.addCSourceFiles(.{ .files = &c_sources, .flags = &[_][]const u8{} });
-    c_lib.step.dependOn(&bison.step);
-
     const yazap = b.dependency("yazap", .{});
     const glob_dep = b.dependency("glob", .{ .target = target, .optimize = optimize });
     const pcre2_dep = b.dependency("pcre2", .{ .target = target, .optimize = optimize });
@@ -87,13 +73,27 @@ pub fn build(b: *std.Build) void {
     translate_pcre.defineCMacro("PCRE2_CODE_UNIT_WIDTH", "8");
     translate_pcre.step.dependOn(&pcre2_dep.artifact("pcre2-8").step);
 
+    const c_lib = b.addLibrary(.{
+        .name = "grok-c",
+        .linkage = .static,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+
+    c_lib.root_module.addIncludePath(b.path(c_code_path));
+    c_lib.root_module.addIncludePath(b.path(generated_path));
+    c_lib.root_module.addCSourceFiles(.{ .files = &c_sources, .flags = &[_][]const u8{} });
+    c_lib.step.dependOn(&bison.step);
+
     const deps = ModuleDeps{
         .b = b,
         .yazap = yazap,
         .glob_dep = glob_dep,
         .pcre2_dep = pcre2_dep,
-        .c_sources = &c_sources,
-        .c_lib = null,
+        .c_lib = c_lib,
         .c_code_path = c_code_path,
         .options = options,
         .translate_c = translate_c,
@@ -134,19 +134,6 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
 
     // Fuzzing
-
-    const fuzz_deps = ModuleDeps{
-        .b = b,
-        .yazap = yazap,
-        .glob_dep = glob_dep,
-        .pcre2_dep = pcre2_dep,
-        .c_sources = &.{},
-        .c_lib = c_lib,
-        .c_code_path = c_code_path,
-        .options = options,
-        .translate_c = translate_c,
-        .translate_pcre = translate_pcre,
-    };
     const fuzzing = b.addTest(.{
         .name = "fuzzing",
         .root_module = b.createModule(.{
@@ -156,7 +143,7 @@ pub fn build(b: *std.Build) void {
             .link_libc = true,
         }),
     });
-    fuzz_deps.applyTo(fuzzing.root_module);
+    deps.applyTo(fuzzing.root_module);
 
     const run_fuzzing = b.addRunArtifact(fuzzing);
     const fuzz_string_step = b.step("fuzzing", "Fuzzing");
@@ -218,8 +205,7 @@ const ModuleDeps = struct {
     yazap: *std.Build.Dependency,
     glob_dep: *std.Build.Dependency,
     pcre2_dep: *std.Build.Dependency,
-    c_sources: []const []const u8,
-    c_lib: ?*std.Build.Step.Compile,
+    c_lib: *std.Build.Step.Compile,
     c_code_path: []const u8,
     options: *std.Build.Step.Options,
     translate_c: *std.Build.Step.TranslateC,
@@ -229,11 +215,7 @@ const ModuleDeps = struct {
         mod.addImport("glob", self.glob_dep.module("glob"));
         mod.addImport("yazap", self.yazap.module("yazap"));
         mod.addIncludePath(self.b.path(self.c_code_path));
-        if (self.c_lib) |lib| {
-            mod.linkLibrary(lib);
-        } else {
-            mod.addCSourceFiles(.{ .files = self.c_sources, .flags = &[_][]const u8{} });
-        }
+        mod.linkLibrary(self.c_lib);
         mod.linkLibrary(self.pcre2_dep.artifact("pcre2-8"));
         mod.addImport("build_options", self.options.createModule());
         mod.addImport("c", self.translate_c.createModule());
