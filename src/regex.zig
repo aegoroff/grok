95b45698adcd42d0dd61c304d9e1c0cef56fd16f
Alexander Egorov <egoroff@gmail.com>
2026-06-19 10:48:04 +0300

    test: fuzzing tests only file mode now. Crashing log test added

--- src/regex.zig
+++ src/regex.zig
@@ -216,6 +216,9 @@ pub fn match(gpa: std.mem.Allocator, prepared: *const Prepared, subject: []const
     const match_ctx = re.pcre2_match_context_create_8(general_context);
     defer re.pcre2_match_context_free_8(match_ctx);
 
+    _ = re.pcre2_set_match_limit_8(match_ctx, 100_000);
+    _ = re.pcre2_set_depth_limit_8(match_ctx, 10_000);
+
     const rc: c_int = re.pcre2_match_8(prepared.re, subject.ptr, subject.len, 0, re.PCRE2_NOTEMPTY, match_data, match_ctx);
     const matched = rc > 0;
 
